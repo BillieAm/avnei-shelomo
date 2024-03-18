20e0618b4ad06186f0b4572e4b9330cecdc337df
Billie Amit <biliamit@gmail.com>
2024-03-18 12:06:59 +0200

    set up the colors variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          DEFAULT: "hsl(var(--background))",
+          200: "hsl(var(--bg-200))",
+          300: "hsl(var(--bg-300))",
+          gradient: "hsl(var(--bg-gradient))"
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          200: "hsl(var(--primary-200))",
+          300: "hsl(var(--primary-300))"
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          200: "hsl(var(--accent-200))"
+        },
+        frontGround: {
+          DEFAULT: "hsl(var(--text))",
+          200: "hsl(var(--text-200))",
+          white: "hsl(var(--text-white))"
+        },
+        titleGradient: {
+          DEFAULT: "hsl(var(--text-gradient))",
+          bg: "hsl(var(--bg-text-gradient))"
+        }
+      }
+    }
+  },
+  plugins: []
+};
