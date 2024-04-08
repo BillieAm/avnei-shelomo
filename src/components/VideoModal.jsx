62b90efce85f43e32f6df3763fd05930b72e3ee0
Billie Amit <biliamit@gmail.com>
2024-04-08 10:33:34 +0300

    add iframe video modal component and conect it to the main video in the home page

---
+++ src/components/VideoModal.jsx
@@ -0,0 +1,66 @@
+import { useRef, useEffect } from "preact/hooks";
+
+import { X } from "lucide-preact";
+
+export default function VideoModal({ contentSrc, btnText, btnClass }) {
+  const dialogRef = useRef(null);
+  const videoRef = useRef(null);
+
+  const openModal = () => {
+    dialogRef.current.showModal();
+  };
+
+  const closeModal = () => {
+    dialogRef.current.close();
+    // stop video when modal closed
+    let iframeSrc = videoRef.current.src;
+    videoRef.current.src = iframeSrc;
+  };
+
+  const handleClickOutside = (event) => {
+    if (event.target === dialogRef.current) {
+      closeModal();
+    }
+  };
+
+  function handleKeydown(event) {
+    if (event.code === "Escape") {
+      closeModal();
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
+  return (
+    <div>
+      <button onClick={openModal} className={btnClass}>
+        {btnText}
+      </button>
+      <dialog
+        ref={dialogRef}
+        onClick={handleClickOutside}
+        className="bg-transparent backdrop:bg-foreground/70"
+      >
+        <button onClick={closeModal} className="mb-4">
+          <X color="white" strokeWidth={4} />
+        </button>
+        <div className="flex">
+          <iframe
+            width="560"
+            height="315"
+            src={contentSrc}
+            title="WizzoStream video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            ref={videoRef}
+          ></iframe>
+        </div>
+      </dialog>
+    </div>
+  );
+}
