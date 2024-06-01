ac4e14ccef445e6397be50e09093b09b4b280b3f
Billie Amit <biliamit@gmail.com>
2024-06-01 22:02:26 +0300

    add video to lessons

--- src/components/LessonsContainer.jsx
+++ src/components/LessonsContainer.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Tabs from "./Tabs";
 import AudioLesson from "./AudioLesson";
 
-export default function LessonsContainer({ translatedTabs, audioFiles }) {
+export default function LessonsContainer({ translatedTabs, tabsContent }) {
   const [selectedTab, setSelectedTab] = useState(translatedTabs[0]);
+  const [tabContent, setTabContent] = useState(tabsContent[0]);
+
+  useEffect(() => {
+    setTabContent(tabsContent[translatedTabs.indexOf(selectedTab)]);
+  }, [selectedTab]);
+
   return (
     <div className="content-wrapper m-8 flex flex-col items-center gap-8 rounded-xl bg-orange-300">
       <Tabs
@@ -13,7 +19,7 @@ export default function LessonsContainer({ translatedTabs, audioFiles }) {
         setActiveTab={setSelectedTab}
       />
       <div className="grid grid-cols-2 gap-6 lg:grid-cols-3">
-        {audioFiles.map((fileObj, index) => (
+        {tabContent.map((fileObj, index) => (
           <AudioLesson key={index} url={fileObj.url} title={fileObj.name} />
         ))}
       </div>
