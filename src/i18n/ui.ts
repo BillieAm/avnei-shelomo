b4aa08fbe6fb21a601e526ad3287d482feca6f73
Billie Amit <biliamit@gmail.com>
2024-08-03 11:45:55 +0300

    add dynamic audio lessons category page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -26,6 +26,7 @@ export const ui = {
     "gallery.album2": "ההתחלה",
     "lessons.video": "וידאו",
     "lessons.audio": "אודיו",
+    "lessons.catBtn": "לשיעורים",
     "donation.title": "בואו לקחת חלק",
     "donation.body": "הצטרפו למעגל החשוב של תומכינו באמצעות תרומה בטופס מאובטח",
     "donation.btn": "לתרומה",
@@ -74,6 +75,7 @@ export const ui = {
     "gallery.album2": "The Beginning",
     "lessons.video": "Video",
     "lessons.audio": "Audio",
+    "lessons.catBtn": "All Lessons",
     "donation.title": "Take Part",
     "donation.body":
       "Join our essential circle of supporters by making a secure donation",
