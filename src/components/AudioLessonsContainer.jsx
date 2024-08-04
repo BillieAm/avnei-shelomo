d0c841670e95c1eda2490c419827c8a02f49f290
Billie Amit <biliamit@gmail.com>
2024-08-04 11:05:36 +0300

    build audio lessons container functioning part 2

--- src/components/AudioLessonsContainer.jsx
+++ src/components/AudioLessonsContainer.jsx
@@ -5,11 +5,17 @@ import AudioLesson from "./AudioLesson";
 
 export default function AudioLessonsContainer({ categoryData }) {
   const isFolder = categoryData.children[0].type === "folder";
-  const tabsList =
-    isFolder && categoryData.children.map((folder) => folder.name);
-  console.log("isFolder", isFolder);
-  console.log("tabsList", tabsList);
+  const tabsList = categoryData.children.map((folder) => folder.name);
+
   const [selectedTab, setSelectedTab] = useState(tabsList[0]);
+  const [currentFolderLessons, setCurrentFolderLessons] = useState(
+    categoryData.children[0].children,
+  );
+
+  useEffect(() => {
+    const index = tabsList.indexOf(selectedTab);
+    setCurrentFolderLessons(categoryData.children[index].children);
+  }, [selectedTab]);
 
   return (
     <div>
@@ -20,24 +26,23 @@ export default function AudioLessonsContainer({ categoryData }) {
             activeTab={selectedTab}
             setActiveTab={setSelectedTab}
           />
+          {currentFolderLessons.map((lesson) => (
+            <AudioLesson
+              key={lesson.id}
+              url={`https://drive.google.com/file/d/${lesson.id}/preview`}
+              title={lesson.name}
+            />
+          ))}
         </div>
       ) : (
-        <div></div>
-      )}
-    </div>
-  );
-}
-
-/* 
- <div>
-      {categoryData.children.map((lesson) =>
-         (
+        categoryData.children.map((lesson) => (
           <AudioLesson
             key={lesson.id}
             url={`https://drive.google.com/file/d/${lesson.id}/preview`}
             title={lesson.name}
           />
-        ),
+        ))
       )}
     </div>
-*/
+  );
+}
