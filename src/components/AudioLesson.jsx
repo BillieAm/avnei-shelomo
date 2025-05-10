c3faaab9f567b2cfb77076650bdf6263dfddcbeb
Billie Amit <billieamit@Billies-MacBook-Pro-2.local>
2025-05-10 16:43:37 +0300

    add the final json data for audio lessons and adapt style

--- src/components/AudioLesson.jsx
+++ src/components/AudioLesson.jsx
@@ -1,7 +1,10 @@
 export default function AudioLesson({ url, title }) {
   return (
-    <article title={title} class="rounded-xl bg-red-400 p-3">
-      <p class="truncate text-center">{title}</p>
+    <article
+      title={title}
+      class="flex flex-col justify-between rounded-xl bg-red-400 p-3"
+    >
+      <p class="text-center">{title}</p>
       <div class="flex justify-center">
         <audio controls>
           <source src={url} type="audio/mpeg" />
