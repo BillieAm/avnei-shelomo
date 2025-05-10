03c89ccbec3678131b2878cc419bc4262d16096a
Billie Amit <billieamit@Billies-MacBook-Pro-2.local>
2025-05-10 22:47:12 +0300

    fix typescript issues with home and about content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,13 +2,13 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const homeInfo = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: "./src/data/home" }),
+  loader: glob({ pattern: "**/*.md", base: "./src/data/home" }),
   schema: z.object({
     title: z.string(),
   }),
 });
 const aboutInfo = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: "./src/data/about" }),
+  loader: glob({ pattern: "**/*.md", base: "./src/data/about" }),
 });
 
-export const collections = {homeInfo, aboutInfo};
+export const collections = { homeInfo, aboutInfo };
